8f52175f6b61ccfe25d629c02a2ad2621029600d
Gyorgy Szombathelyi <gyurco@freemail.hu>
2019-06-02 19:37:22 +0200

    Revert "Adjust cache controller for the changed SDRAM controller"
    This reverts commit a9ff6d7deba9466e84de4da34e60a24acc337dee.

--- src/TwoWayCache.v
+++ src/TwoWayCache.v
@@ -333,7 +333,7 @@ begin
 			begin
 				sdram_req<=1'b0;
 				data_to_cpu <= data_from_sdram;
-				//cpu_ack<=1'b1; // Too soon?
+				cpu_ack<=1'b1; // Too soon?
 
 				// write first word to Cache...
 				data_ports_w<={2'b11,data_from_sdram};
@@ -345,7 +345,7 @@ begin
 
 		FILL2:
 		begin
-			cpu_ack<=1'b1; // Maintain ack signal if necessary
+			//cpu_ack<=cpu_req; // Maintain ack signal if necessary
 			// write second word to Cache...
 			readword_burst<=1'b1;
 			readword<=readword+1'b1;
@@ -357,7 +357,7 @@ begin
 
 		FILL3:
 		begin
-			//cpu_ack<=1'b1; // Maintain ack signal if necessary
+			//cpu_ack<=cpu_req; // Maintain ack signal if necessary
 			// write third word to Cache...
 			readword_burst<=1'b1;
 			readword<=readword+1'b1;
@@ -382,8 +382,8 @@ begin
 		begin
 			//cpu_ack <= 1'b1;
 			readword<=cpu_addr[2:1];
-			state<=WAITING;
-//			state<=PAUSE1; // Allow one extra clock after clearing readword_burst
+//			state<=WAITING;
+			state<=PAUSE1; // Allow one extra clock after clearing readword_burst
 		end
 
 		default:
